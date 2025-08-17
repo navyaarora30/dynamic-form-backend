b0d91689df866203fa01c90a67570860c4627621
Navya Arora <navyaarora625@gmail.com>
2025-08-17 14:25:26 +0530

    Auto-generate slug from title

--- server.js
+++ server.js
@@ -3,25 +3,42 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import formRoutes from "./routes/formRoutes.js";
-import authRoutes from "./routes/authRoutes.js"; 
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  "https://dynamic-form-frontend-rust.vercel.app",
+  "http://localhost:5173" // optional: for local dev
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Form Builder API is working");
 });
 
-app.use("/api", formRoutes);         
-app.use("/api/auth", authRoutes);    
+app.use("/api", formRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   })
   .then(() => {
     console.log("MongoDB connected");
